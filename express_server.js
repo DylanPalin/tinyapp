d95a2e312ea137783ed3766ff7b25b6da918c54d
Dylan Palin <dylanpalin@gmail.com>
2023-11-28 20:40:57 -0700

    added post route for new longurl submission

--- express_server.js
+++ express_server.js
@@ -3,11 +3,17 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const randomString = Math.random().toString(36).substring(2, 8);
+  return randomString;
+}
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -16,6 +22,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
